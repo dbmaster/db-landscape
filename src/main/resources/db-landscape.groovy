68f1b275ed8097fa7556dcad12220b146bf3cd13
Slava Christin <slava@branegy.com>
2015-01-25 16:32:53 -0500

    Formatting. No code changes

--- src/main/resources/db-landscape.groovy
+++ src/main/resources/db-landscape.groovy
@@ -10,7 +10,7 @@ import com.branegy.service.base.api.ProjectService
 
 
 def emptystr(obj) {
- return obj==null ? "" : obj;
+    return obj==null ? "" : obj;
 }
 
 
@@ -32,45 +32,43 @@ def environments = [] as Set
 def dbGrid = new TreeMap()
 
 def mapDbToApp =  { databaseGrid, appName, database, environment ->
-
-            def dbByEnv = databaseGrid[appName] 
-            if (dbByEnv == null ) { 
-	        dbByEnv = [:] 
-                databaseGrid[appName] = dbByEnv
-            }
-            def dbList = dbByEnv[environment] 
-            if (dbList == null) {
-               dbList = []
-               dbByEnv[environment]  = dbList
-            }
-            dbList << database
+    def dbByEnv = databaseGrid[appName] 
+    if (dbByEnv == null ) { 
+        dbByEnv = [:] 
+        databaseGrid[appName] = dbByEnv
+    }
+    def dbList = dbByEnv[environment] 
+    if (dbList == null) {
+        dbList = []
+        dbByEnv[environment]  = dbList
+    }
+    dbList << database
 }
 
 def appList = null;
 if (p_app_filter!=null) { 
-   appList = inventorySrv.getApplicationList(new QueryRequest(p_app_filter))
+    appList = inventorySrv.getApplicationList(new QueryRequest(p_app_filter))
 }
 
 
 for (Database database: inventoryDBs) {
     if (!database.isDeleted()) {
-       def environment = database.getCustomData("Environment") ?: "Undefined"
+        def environment = database.getCustomData("Environment") ?: "Undefined"
 
-       def apps = dbApps[database];
+        def apps = dbApps[database];
         
-       if (apps!=null && apps.size() >0 ) {
-           apps.each { 
-              def appName = it.getApplication()?.getApplicationName() ?: "Undefined"
+        if (apps!=null && apps.size() >0 ) {
+            apps.each { 
+                def appName = it.getApplication()?.getApplicationName() ?: "Undefined"
 
-              if (appList == null || appList.find { app  -> app.getApplicationName().equals(appName) } !=null) { 
-                 environments << environment
-                 mapDbToApp (dbGrid, appName, database, environment )
-              }
-
-           }
-       } else if (appList == null) {
-           environments << environment
-           mapDbToApp (dbGrid, "UnAssigned", database , environment )
+                if (appList == null || appList.find { app  -> app.getApplicationName().equals(appName) } !=null) { 
+                    environments << environment
+                    mapDbToApp (dbGrid, appName, database, environment )
+                }
+            }
+        } else if (appList == null) {
+            environments << environment
+            mapDbToApp (dbGrid, "UnAssigned", database , environment )
        }
     }
 }
@@ -89,22 +87,20 @@ String.metaClass.encodeURL = { java.net.URLEncoder.encode(delegate) }
 
 String projectName =  dbm.getService(ProjectService.class).getCurrentProject().getName()
 
-
-
 dbGrid.each {
-        println "<tr style=\"vertical-align: top;\"><td>${it.key}</td>"
-        environments.each { env ->
-           dbList = it.value[env]
-           println "<td style=\"padding:5px\">"
-           if (dbList!=null) {
-             dbList.each { db ->
+    println "<tr style=\"vertical-align: top;\"><td>${it.key}</td>"
+    environments.each { env ->
+        dbList = it.value[env]
+        println "<td style=\"padding:5px\">"
+        if (dbList!=null) {
+            dbList.each { db ->
                 def link = "#inventory/project:${toURL(projectName)}/databases/server:${toURL(db.getServerName())},db:${toURL(db.getDatabaseName())}/applications"
                 println "<a href=\"${link}\">${db.getServerName()}.${db.getDatabaseName()}</a><br/>"
-             }
-           }
-           println "</td>"
+            }
         }
-        println "</tr>"
+        println "</td>"
+    }
+    println "</tr>"
 }
 
 println "</table>"
