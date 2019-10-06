91396451fe572c0f78eb024b9b1e74e157214596
vitalh <vitaly@branegy.com>
2019-10-06 18:03:34 +0200

    update test params

--- src/test/groovy/DbLandscapeIT.groovy
+++ src/test/groovy/DbLandscapeIT.groovy
@@ -1,9 +1,8 @@
-import io.dbmaster.testng.BaseToolTestNGCase;
-
 import static org.testng.Assert.assertTrue;
+
 import org.testng.annotations.Test
 
-import com.branegy.tools.api.ExportType;
+import io.dbmaster.testng.BaseToolTestNGCase;
 
 
 public class DbLandscapeIT extends BaseToolTestNGCase {
@@ -11,6 +10,9 @@ public class DbLandscapeIT extends BaseToolTestNGCase {
     @Test
     public void test() {
         def parameters = [ : ]
+        parameters.put("p_exlcude_undefined":false);
+        parameters.put("p_include_objects":["Servers","Databases","Jobs","SecurityObjects","Contacts"]);
+        
         String result = tools.toolExecutor("db-landscape", parameters).execute()
         assertTrue(result.contains("Application"), "Unexpected search results ${result}");
         assertTrue(result.contains("Undefined"), "Unexpected search results ${result}");
